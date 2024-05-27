ca10d5e2f3e32711ff0d34b167fd2fbbdeb162c5
subashini <subashini93825@gmail.com>
2024-05-27 10:39:58 +0530

    SideBar component added

---
+++ src/Components/SideBar/SideBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div>
+        <div>
+            <ol>
+                <a>
+                   <li>
+
+                   </li>
+                </a>
+                <a>
+                   <li>
+                    
+                   </li>
+                </a>
+                <a>
+                   <li>
+                    
+                   </li>
+                </a>
+                <a>
+                   <li>
+                    
+                   </li>
+                </a>
+            </ol>
+           
+        </div>
+    </div>
+  )
+}
+
+export default SideBar
